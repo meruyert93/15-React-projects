593489195e75f6f1a295f3a8822cf3e81ef1ebf4
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-03 20:44:24 +0100

    birthday-reminder

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,29 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+    const[people, setPeople] = useState(data);
+    const removeItem = (id) => {
+        const remainPeople = people.filter((person) => person.id !== id);
+        setPeople(remainPeople);
+    }
+
+    return (
+        <main>
+            <section className="container">
+                <h3>{people.length} Birthdays Today</h3>
+                <List people={people} removeItem={(id) => removeItem(id)}>
+                </List>
+                <button onClick={() => setPeople([])}>
+                    clear all
+                </button>
+                <button onClick={() => setPeople(data)}>
+                    return List
+                </button>
+            </section>
+        </main>
+     )
 }
 
 export default App;

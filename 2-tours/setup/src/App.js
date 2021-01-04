1ab747e2020ddca236e1cad7eefe8fba856be445
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-04 19:41:56 +0100

    Initial Setup for Second Project

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -4,7 +4,10 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
+    fetch(url)
+        .then(response => response.json()) 
   return <h2>Tours Project Setup</h2>
 }
 

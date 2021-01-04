3f8dc2fc92827863938e3384c04a817bd49e4fc1
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-04 22:03:20 +0100

    2-tours: adding tour info

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
     return (
-        <article>Single tour component</article>);
+        <article className="single-tour">
+        <img src={image} alt={name}/>
+        <footer>
+            <div className="tour-info">
+                <h4>{name}</h4>
+                <h4 className="tour-price">{price}</h4>
+            </div>
+            <p>
+                {info}
+            </p>
+            <button className="delete-btn">
+                Not Interested
+            </button>
+        </footer>
+        </article>);
 };
 
 export default Tour;

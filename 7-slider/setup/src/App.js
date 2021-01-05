f48f96982c5e045223e17003b13a22e91bfe3604
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-05 23:20:59 +0100

    7-slider: slider classes

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -19,8 +19,15 @@ function App() {
                 {people.map((person, personIndex) => {
                     const {id, image, name, title, quote} =  person;
                     //more stuff coming up
+                    let position = 'nextSlide'
+                    if (personIndex === index) {
+                        position = 'activeSlide';
+                    }
+                    if (personIndex === index - 1 || (index === 0 && personIndex === index - 1)) {
+                        position = 'lastSlide';
+                    }
                     return (
-                        <article key={id}>
+                        <article  className={position}key={id}>
                             <img src={image} alt={name} className="person-img"/>
                             <h4>{name}</h4>
                             <p className="title">{title}</p>
@@ -29,10 +36,10 @@ function App() {
                         </article>
                     )
                 })}
-                <button className="prev">
+                <button className="prev" onClick={() => setIndex(index - 1)}>
                     <FiChevronLeft/>
                 </button>
-                <button className="next">
+                <button className="next" onClick={() => setIndex(index + 1)}>
                     <FiChevronRight/>
                 </button>
             </div>

1950f1873024924508a8e8523a11be0007fa1be8
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-05 18:51:55 +0100

    5-menu: categories with new Set

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
+const Categories = ({filterItems, categories}) => {
     return (
         <div className="btn-container">
+            {categories.map((category, index)=>{
+                return (
+                    <button type="button" className="filter-btn" key={index} onClick={() => {filterItems(category)}}>
+                        {category}
+                    </button>
+                )
+            })}
+            {/* <button className="filter-btn" onClick={() => {filterItems('all')}}>
+                all
+            </button>
             <button className="filter-btn" onClick={() => {filterItems('breakfast')}}>
                 breakfast
-            </button>
+            </button> */}
         </div>
     );
 };

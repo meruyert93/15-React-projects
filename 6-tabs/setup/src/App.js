34764f77ed1a3eab8ffd77dd3b3aa15a0f47789e
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-05 19:25:49 +0100

    6-tabs: Fetching Data

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -4,7 +4,36 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+    const [loading, setLoading] = useState(true);
+    const [job, setJobs] = useState([]);
+    const [value, setValue] = useState(0);
+
+    const fetchJob = async () => {
+        const response = await fetch(url);
+        const newJobs = await response.json();
+        setJobs(newJobs)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchJob();
+    }, []);
+
+    if(loading) {
+        return(
+            <section className="section loading">
+                <h1>Loading...</h1>
+            </section>
+        )
+    }
+
+    return (
+        <main>
+            <section>
+                <h2>Jobs</h2>
+            </section>
+        </main>
+    )
 }
 
 export default App

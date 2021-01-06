33e6baa023008596996b2777fc6a4c1845d12839
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-06 23:38:43 +0100

    8-lorem-ipsum: Styling and Input Functionality

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
   
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
+  const submitData = (e) =>{
+    e.preventDefault();
+    console.log(count);
+  }
+
+    return (
+        <section className="section-center">
+            <h3>tired of boring lorem ipsum?</h3>
+            <form className="lorem-form">
+                <label htmlFor="amount">paragraphs:</label>
+                <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)}/>
+                <button type="submit" className="btn" onClick={submitData}>generate</button>
+            </form>
+            <article className="lorem-text">
+                {
+
+                }
+            </article>
+        </section>
     )
 }
 

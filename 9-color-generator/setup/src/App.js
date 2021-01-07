81c009dc8f3a25b410e93932dce2cb8521232774
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-07 23:32:29 +0100

    9-color-generator: color palette

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,20 +6,20 @@ import Values from 'values.js'
 function App() {
     const [color, setColor] = useState('');
     const [error, setError] = useState(false);
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(new Values('#8A2BE2').all(10));
 
     const handleSubmit = (e) => {
         e.preventDefault();
         try {
             let colors = new Values(color).all(10)
-            console.log(colors);
+            setList(colors)
+            //console.log(colors);
         } catch(error) {
             setError(true);
-            console.log(error)
+            //console.log(error)
         }
     
     }
-
 
     return (
         <>
@@ -37,9 +37,9 @@ function App() {
                 </form>
             </section>
             <section className="colors">
-                <h4>
-                    List goes here
-                </h4>
+                {list.map((color, index) => {
+                    return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+                })}
             </section>
         </>
 )

b6cc69fec78e179412505201a7674c5dbcd4a3a4
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-10 00:45:26 +0100

    11-Navbar: useRef Approach for Toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,6 +5,19 @@ import logo from './logo.svg'
 
 const Navbar = () => {
     const [showLinks, setShowLinks] = useState(false)
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    useEffect(()=>{
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`
+        }
+        else {
+            linksContainerRef.current.style.height = '0px'
+        }
+    }, [showLinks])
+
     return (
         <nav className="nav-center">
             <div className="nav-header">
@@ -13,8 +26,8 @@ const Navbar = () => {
                     <FaBars/>
                 </button>
             </div>
-                <div className={`${showLinks ? 'links-container show-container' : 'links-container'}`}>
-                <ul className="links">
+                <div className='links-container' ref={linksContainerRef}>
+                <ul className="links" ref={linksRef}>
                     {links.map((link) => {
                         const {id, url, text} = link;
                         return <li key={id}>
@@ -22,7 +35,6 @@ const Navbar = () => {
                                     {text}
                                 </a>
                         </li>
-
                     })}
                 </ul>
             </div>

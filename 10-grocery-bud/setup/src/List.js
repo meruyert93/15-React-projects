df896839173a14b9f0f612312bd754792a757014
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-09 15:32:53 +0100

    10-grocery-bud: inifial form

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 const List = () => {
-  return <h2>list component</h2>
+    return (
+        <h2>
+            list component
+        </h2>
+    )
 }
 
 export default List

0fd07a367127be3672252dc1ebf073835d3f8ee3
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-10 17:14:16 +0100

    12-Sidebar-Modal: Custom hook and Context API

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import {AppContext, useGlobalContext} from './context'
 
 const Home = () => {
+    const {openSidebar, openModal} = useGlobalContext();
+    
     return (
         <main>
-            <button className="sidebar-toggle">
+            <button className="sidebar-toggle" onClick={openSidebar}>
                 <FaBars/>
             </button>
-            <button className="btn">
+            <button className="btn" onClick={openModal}>
                 show modal
             </button>
         </main>
